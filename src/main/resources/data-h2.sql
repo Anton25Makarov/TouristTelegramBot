dd461c4f1a30afacb2e87c5176f7b835317b92c3
Anton Makarau <anton.makarau@thomsonreuters.com>
2020-03-22 14:07:57 +0300

    add real descriptions to cities

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,7 +1,7 @@
 -- CITIES
 INSERT INTO "TOURIST"."CITY" (NAME, DESCRIPTION)
-VALUES ('Minsk', 'Minsk is a great city'),
-       ('Vitebsk', 'Vitebsk is...'),
-       ('Grodno', 'Grodno is...'),
-       ('Gomel', 'Gomel is...'),
-       ('Mogilev', 'Mogilev is...');
+VALUES ('Minsk', 'Minsk is the capital and largest city of Belarus, situated on the Svislač and the Nyamiha Rivers.'),
+       ('Vitebsk', 'Vitebsk  is a city in Belarus. The capital of the Vitebsk Region, it had 342,381 inhabitants in 2004, making it the country''s fourth-largest city.'),
+       ('Grodno', 'Grodno is a city in western Belarus. It is located on the Neman close to the borders of Poland and Lithuania'),
+       ('Gomel', 'Gomel s the administrative centre of Gomel Region and with 526,872 inhabitants the second-most populous city of Belarus.'),
+       ('Mogilev', 'Mogilev is a city in eastern Belarus, on the Dnieper River, about 76 kilometres from the border with Russia''s Smolensk Oblast and 105 km from the border with Russia''s Bryansk Oblast.');
