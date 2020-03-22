e23529db26a7252a9c5d59c74cc42cedebe01463
Anton Makarau <anton.makarau@thomsonreuters.com>
2020-03-22 19:43:57 +0300

    add response status annpotations under exceptions

--- src/main/java/com/telegram/tourist/exception/AlreadyExistsException.java
+++ src/main/java/com/telegram/tourist/exception/AlreadyExistsException.java
@@ -1,5 +1,9 @@
 package com.telegram.tourist.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class AlreadyExistsException extends RuntimeException {
 
   public AlreadyExistsException(String message) {
